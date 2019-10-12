dfc328a3877e9be493832809667927234222f264
Uriel Espinoza <urielintemperie@gmail.com>
2019-10-12 03:21:20 -0300

    asvfvasf

--- pages/index.js
+++ pages/index.js
@@ -2,4 +2,13 @@ import React from 'react'
 
 export default function index(){
   return(<h1>Home</h1>)
+/*
+  return(
+  <div>
+    <h1>Loginx </h1>
+    <p>Esto son mas cosas</p>
+  </div>
+  
+  )
+*/
 }
