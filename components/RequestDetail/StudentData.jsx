f5fd70fac78049a52a9c3b2b62c512984eb2cdf1
camilacintioli <camila.cintioli@gmail.com>
2019-10-12 12:22:27 -0300

    Add paper with student data

--- components/RequestDetail/StudentData.jsx
+++ components/RequestDetail/StudentData.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
+
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 
-export default function StudentData() {
-    return (
-    <>
-    <Typography variant="h5">100/07</Typography>
-    <Typography variant="h5">Uriel Espinoza</Typography>
-    <Typography variant="h5">Universidad Tecnológica Nacional</Typography>
-    <Typography variant="h5">Ingenieria en Sistemas</Typography>
-    </>)
-}
+const useStyles = makeStyles(theme => ({
+    root: {
+        padding: theme.spacing(3, 2),
+    },
+}));
 
+export default function StudentData() {
+    const classes = useStyles();
+
+    return (
+        <div>
+            <Paper className={classes.root}>
+                <Typography variant="h5">100/07</Typography>
+                <Typography variant="h5">Uriel Espinoza</Typography>
+                <Typography variant="h7">Universidad Tecnológica Nacional</Typography> - 
+                <Typography variant="h7">Ingenieria en Sistemas</Typography>
+            </Paper>
+        </div>
+    );
+}
